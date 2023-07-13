8c681b7c9531170006e3e7b5c737dad89d8d7cf3
Vivek Nair <vivekmanju53@gmail.com>
2023-07-13 19:26:03 +0530

    window gui fix

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from tkinter.messagebox import showinfo
 from docx2pdf import convert
 
 win = tk.Tk()
-win.title("Word to Pdf Converter App")
+win.title("Word to PDF Converter App")
+win.geometry("400x200")
+win.resizable(False, False)
 
 def openfile():
     file = askopenfile(filetypes=[('Word Files', '*.docx')])
@@ -15,14 +17,19 @@ def openfile():
         showinfo("Done", "File successfully converted")
         progress_label.config(text="Conversion completed")
 
+label = ttk.Label(win, text="Choose a Word file:")
+label.grid(row=0, column=0, padx=10, pady=10, sticky="w", columnspan=2)
 
-label = tk.Label(win, text="Choose a file!")
-label.grid(row=10, column=5, padx=5, pady=5)
+button = ttk.Button(win, text="Select", command=openfile)
+button.grid(row=1, column=0, padx=10, pady=10, sticky="w", columnspan=2)
 
-button = ttk.Button(win, text="Select", width=30, command=openfile)
-button.grid(row=20, column=5, padx=5, pady=5)
+progress_label = ttk.Label(win, text="")
+progress_label.grid(row=2, column=0, padx=10, pady=10, sticky="w", columnspan=2)
 
-progress_label = tk.Label(win, text="")
-progress_label.grid(row=30, column=5, padx=5, pady=5)
+# Centering the widgets vertically and horizontally
+win.grid_columnconfigure(0, weight=1)
+win.grid_columnconfigure(1, weight=1)
+win.grid_rowconfigure(0, weight=1)
+win.grid_rowconfigure(3, weight=1)
 
 win.mainloop()

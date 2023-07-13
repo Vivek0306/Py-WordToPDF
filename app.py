8b94b10ea61736c0ed76e8f35b57d1928f97d5b3
Vivek Nair <vivekmanju53@gmail.com>
2023-07-13 19:14:56 +0530

    fixed file selector

--- app.py
+++ app.py
@@ -4,19 +4,25 @@ from tkinter.filedialog import askopenfile
 from tkinter.messagebox import showinfo
 from docx2pdf import convert
 
-win=tk.Tk()
+win = tk.Tk()
 win.title("Word to Pdf Converter App")
 
 def openfile():
-  file = askopenfile(filetypes=[('Word Files', '*.docx')])
-  convert(file.name, 'converted.pdf')   
-  showinfo("Done", "File successfully converted ")
+    file = askopenfile(filetypes=[('Word Files', '*.docx')])
+    if file:
+        progress_label.config(text="Converting...")
+        convert(file.name, 'converted.pdf')
+        showinfo("Done", "File successfully converted")
+        progress_label.config(text="Conversion completed")
 
-label=tk.Label(win,text="Choose a file!")
-label.grid(row=10,column=5,padx=5,pady=5)  
 
+label = tk.Label(win, text="Choose a file!")
+label.grid(row=10, column=5, padx=5, pady=5)
 
-button=ttk.Button(win,text="Select",width=30,command=openfile())
-button.grid(row=20,column=5,padx=5,pady=5)
+button = ttk.Button(win, text="Select", width=30, command=openfile)
+button.grid(row=20, column=5, padx=5, pady=5)
+
+progress_label = tk.Label(win, text="")
+progress_label.grid(row=30, column=5, padx=5, pady=5)
 
 win.mainloop()
